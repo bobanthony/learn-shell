2d73d456b721463d7f91babb3059c7b4bf8512d2
Dinesh <satyadineshk328@gmail.com>
2023-03-01 12:13:00 +0530

    updating ./learn-shell

--- 04-function.sh
+++ 04-function.sh
@@ -5,12 +5,16 @@ xyz() {
   echo second Argument -- $2
   echo All Arguments - $*
   echo No of Arguments - $#
+  echo value of a - $a
+  b=50
 }
 
 
 
 
 
-
+# main program
 #call a function
 xyz 123 456
+a-150
+echo value of b - $b
