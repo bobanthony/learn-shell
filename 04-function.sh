e3bff6cb564ec2fb140a7c6c847ba754f9d96217
Dinesh <satyadineshk328@gmail.com>
2023-03-01 12:07:23 +0530

    updating ./learn-shell

--- 04-function.sh
+++ 04-function.sh
@@ -1,6 +1,16 @@
 # declare a function
 xyz() {
   echo hello from a function
+  echo first Argument - $1
+  echo second Argument -- $2
+  echo All Arguments - $*
+  echo No of Arguments - $#
 }
+
+
+
+
+
+
 #call a function
-xyz
+xyz 123 456
